3fd1fa3e6a21827a4d41f0960fff451ceee7c107
roushan-sheik <roushansheik@gmail.com>
2024-03-25 23:58:16 +0600

    button component added

---
+++ src/components/button/Btn.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+import { Button } from "@material-tailwind/react";
+import PropTypes from "prop-types";
+import React from "react";
+
+const Btn = ({ label, onClick, color, style, variant }) => {
+  return (
+    <Button
+      className={` ${style}`}
+      onClick={onClick}
+      color={color ? color : ""}
+      variant={variant}
+    >
+      {label || "Btn"}
+    </Button>
+  );
+};
+Btn.propTypes = {
+  label: PropTypes.string,
+  onClick: PropTypes.func,
+  color: PropTypes.string,
+  style: PropTypes.string,
+  variant: PropTypes.string,
+};
+
+export default Btn;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,16 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
+import Btn from "../../components/button/Btn";
 
 const Home = () => {
-  return <div>Home</div>;
+  function handleClick() {
+    alert("clicked");
+  }
+  return (
+    <div>
+      <Btn onClick={handleClick} style={"bg-[#41C9E2] text-black"} />
+    </div>
+  );
 };
 
 export default Home;
