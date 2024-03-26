8f2f09a849e1eee00d47dd45ec5b30b25588d085
roushan-sheik <roushansheik@gmail.com>
2024-03-26 15:01:13 +0600

    custom hook data fetching added

--- src/components/books/Books.jsx
+++ src/components/books/Books.jsx
@@ -1,8 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-
+import useFetchData from "../../hooks/useFetchData";
 const Books = () => {
-  return <div>Books</div>;
+  const { books, loading } = useFetchData();
+  console.log("loading...", loading);
+  console.log("Books", books);
+  return <div></div>;
 };
 
 export default Books;
