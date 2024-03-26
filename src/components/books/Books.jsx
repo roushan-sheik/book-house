4efe3b8e41be8d99a26daaeec0d62440c40a83cf
roushan-sheik <roushansheik@gmail.com>
2024-03-26 13:16:58 +0600

    JSON Data added

---
+++ src/components/books/Books.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Books = () => {
+  return <div>Books</div>;
+};
+
+export default Books;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import Banner from "../../components/banner/Banner";
+import Books from "./../../components/books/Books";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <Books />
     </div>
   );
 };
