86702cb9490f7042ec9bff93211073ae0bdd7270
roushan-sheik <roushansheik@gmail.com>
2024-03-28 00:19:32 +0600

    sorting functionality added

--- src/pages/listed-books/ListedBooks.jsx
+++ src/pages/listed-books/ListedBooks.jsx
@@ -7,6 +7,8 @@ import SortingMenu from "../../components/sorting-menu/SortingMenu";
 import { getBookStorage } from "../../utils/loacla-storage";
 
 const ListedBooks = () => {
+  const [sortedWish, setSortedWish] = React.useState([]);
+  const [sortedRead, setSortedRead] = React.useState([]);
   const [wishlist, setWishlist] = React.useState();
   const [read, setRead] = React.useState();
   const wishlistStore = getBookStorage("wishlist-store");
@@ -32,13 +34,29 @@ const ListedBooks = () => {
       });
       setWishlist(storeData);
       setRead(readData);
+      setSortedRead(readData);
+      setSortedWish(storeData);
     }
     fetchData();
   }, []);
+
   // sort: all , rating, pages, publish
-  function handleSort(val) {
-    alert(val);
+  function handleSort(clickType) {
+    if (clickType == "rating") {
+      setSortedRead([...read.sort((a, b) => b.rating - a.rating)]);
+      setSortedWish([...wishlist.sort((a, b) => b.rating - a.rating)]);
+    } else if (clickType == "pages") {
+      setSortedRead([...read.sort((a, b) => b.page - a.page)]);
+      setSortedWish([...wishlist.sort((a, b) => b.page - a.page)]);
+    } else if (clickType == "publish") {
+      setSortedRead([...read.sort((a, b) => b.publishYear - a.publishYear)]);
+      setSortedWish([
+        ...wishlist.sort((a, b) => b.publishYear - a.publishYear),
+      ]);
+    }
   }
+  console.log("Read", sortedRead);
+  console.log("Wish", sortedWish);
 
   return (
     <div>
@@ -64,14 +82,16 @@ const ListedBooks = () => {
 
             <TabPanel>
               <div className="flex flex-col gap-6 pt-6">
-                {read &&
-                  read.map((book) => <ListBook book={book} key={book.id} />)}
+                {sortedRead &&
+                  sortedRead.map((book) => (
+                    <ListBook book={book} key={book.id} />
+                  ))}
               </div>
             </TabPanel>
             <TabPanel>
               <div className="flex flex-col gap-6 pt-6">
-                {wishlist &&
-                  wishlist.map((book) => (
+                {sortedWish &&
+                  sortedWish.map((book) => (
                     <ListBook book={book} key={book.id} />
                   ))}
               </div>
