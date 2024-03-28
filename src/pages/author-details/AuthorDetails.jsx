02623aef978028c72039f27c4a688f72ff861ca9
roushan-sheik <roushansheik@gmail.com>
2024-03-28 15:17:16 +0600

    author details added and see all authors added

--- src/pages/author-details/AuthorDetails.jsx
+++ src/pages/author-details/AuthorDetails.jsx
@@ -1,25 +1,76 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import { useParams } from "react-router-dom";
+import { IoBookOutline } from "react-icons/io5";
+import { Link, useParams } from "react-router-dom";
+import Btn from "../../components/button/Btn";
+/* eslint-disable no-unused-vars */
 
 const AuthorDetails = () => {
   const { authorName } = useParams();
   const [authorDetails, setAuthorDetails] = React.useState([]);
+
   React.useEffect(() => {
     async function fetchWriters() {
-      const res = await fetch("/writers.json");
-      const data = await res.json();
-      setAuthorDetails(data);
+      try {
+        const res = await fetch("/writers.json");
+        const data = await res.json();
+        console.log(data);
+        setAuthorDetails(data);
+      } catch (error) {
+        console.log("Something went wrong");
+        console.log(error);
+      }
     }
     fetchWriters();
   }, []);
   const author = authorDetails.filter((author) => author.name == authorName);
-  const { id, name, image, country, birthday, books } = author[0];
 
+  const { id, name, image, country, birthday, books, description } =
+    author[0] || {};
+  console.log(name);
   return (
     <div>
-      <div>
-        <img src="" alt="" />
+      <div className="flex flex-col lg:flex-row gap-12 mb-20 ">
+        {/* image box  */}
+        <div className="flex-1 flex bg_third justify-center items-center rounded-2xl 2xl:p-[73px] lg:p-16 p-4">
+          <div className="object-cover w-[425px] lg:h-[564px]  shadow-xl">
+            <img className="w-full h-full" src={image} alt="Hi" />
+          </div>
+        </div>
+        {/* content box  */}
+        <div className="flex-1">
+          <div>
+            <h2 className="text-2xl font-bold text_pri mt-4 mb-1 ">{name}</h2>
+            <h2 className="text-lg font-medium text_sec mb-1">
+              Country: {country}
+            </h2>
+            <p className="text_third mb-4 ">Birth: {birthday}</p>
+            <p className="text_third  font-bold text-base ">Books:</p>
+            <div className="mb-4">
+              {books?.map((book) => {
+                return (
+                  <div
+                    className="text_third flex gap-2 items-center"
+                    key={(id, book)}
+                  >
+                    <IoBookOutline />
+                    <p>{book}</p>
+                  </div>
+                );
+              })}
+            </div>
+            <p className="text_third mb-4 ">
+              <span className="text_third  font-bold text-base ">About:</span>{" "}
+              {description}
+            </p>
+          </div>
+          <Link to={"/writers"}>
+            <Btn
+              label={"See all Authors"}
+              style={"bg_pri mt-6 text-white capitalize text-base"}
+            />
+          </Link>
+        </div>
       </div>
     </div>
   );
