112a1172c1f940ade4b863f9f9e041b6e82344b2
roushan-sheik <roushansheik@gmail.com>
2024-03-28 21:19:48 +0600

    banner view the list button route added

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { Typography } from "@material-tailwind/react";
 import React from "react";
+import { Link } from "react-router-dom";
 import { bannerData } from "../../constants/bannerData";
 import Btn from "./../button/Btn";
 const Banner = () => {
@@ -15,12 +16,14 @@ const Banner = () => {
         >
           {title}
         </Typography>
-        <Btn
-          label={btn}
-          style={
-            "bg_pri lg:py-4 py-2 capitalize text-white w-[190px] 2xl:text-5 lg:text-4 text-base  "
-          }
-        />
+        <Link to={"/listed-books"}>
+          <Btn
+            label={btn}
+            style={
+              "bg_pri lg:py-4 py-2 capitalize text-white w-[190px] 2xl:text-5 lg:text-4 text-base  "
+            }
+          />
+        </Link>
       </div>
       <div className="flex-1 flex justify-center items-center">
         <div className=" 2xl:w-[400px] lg:w-[300px] w-[300px] 2xl:h-[400px] h-[300px] object-cover">
